5df65d7e30fac11c5da7e244d184e1a576eaf698
Tamara Rojas <tamy25105@gmail.com>
2024-06-28 01:03:54 -0400

    logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>BLOG USM</title>
+        <link rel="icon" type="image/png" href="{{ asset('img/cccccccc.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
